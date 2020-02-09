51d898eaa4bfdbe266eb0e5e19c086c4a267d22a
spicybuffal0 <leobardo.delgado@my.tccd.edu>
2020-02-09 18:18:21 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//Leobardo Delgado COSC_1436_57002 using Repl.it IDE
+//Summary of program 
+/*Scenario: Create a C++ Program to accept input from the user while the value entered does not equal -1 (the exit value).
+
+o   If any number other than -1 is entered by the user output to the screen
+
+o   The number entered times itself (n*n) Make this variable of type Double
+
+o   The number entered divided by itself (n/n) Make this Variable of Type Double
+
+o Also Output  (n+3)/5
+
+o  Also Output  [ ( (n+3)/5 ) + ( (n+7)/2 ) ]
+
+Initialized a counter variable called counter to 0
+Increment counter by 1 with each loop of your program
+Output the value of the counter to the screen of the users after the program exits. */
+
+#include <iostream>
+using namespace std;
+int main() 
+ 
+
+{
+
+
+}
